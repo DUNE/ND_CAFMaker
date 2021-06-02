b905b1d7a0d9978277cb953bf4bde1a41f3e2573
Christopher Backhouse <bckhouse@fnal.gov>
2021-06-02 12:14:14 -0500

    Force nusystematics v00_04_01 in build.sh. This is the last version compatible with genie2.
    Add a -f ("force") option to build.sh to clear out previous check outs.
    
    Run builds in parallel.

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
 TOPDIR=${PWD}
+
+FORCE=no
+if [[ $# == 1 && x$1 == x-f ]]; then FORCE=yes; fi
 
 # set up software
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
@@ -18,6 +23,7 @@ G4_cmake_file=`find ${GEANT4_FQ_DIR}/lib64 -name 'Geant4Config.cmake'`
 export Geant4_DIR=`dirname $G4_cmake_file`
 
 # Get edep-sim and build it
+if [ $FORCE == yes ]; then rm -rf edep-sim; fi
 git clone https://github.com/ClarkMcGrew/edep-sim.git
 cd edep-sim
 sed -i 's/add_definitions(-DEDEPSIM_FORCE_PRIVATE_FIELDS)//g' */CMakeLists.txt
@@ -28,21 +34,25 @@ cd ${TOPDIR}
 
 # Get nusystematics and built it "artless"
 # The ART-dependent version can be ups setup but for ND we need this special build
-git clone ssh://p-nusystematics@cdcvs.fnal.gov/cvs/projects/nusystematics
+#
+# v00_04_01 is the last version to depend on genie v2 (specifically v2_12_10d)
+if [ $FORCE == yes ]; then rm -rf nusystematics; fi
+git clone ssh://p-nusystematics@cdcvs.fnal.gov/cvs/projects/nusystematics -b v00_04_01
 mkdir nusystematics/build
 cd nusystematics/build
 cmake ../ -DUSEART=0 -DLIBXML2_LIB=/cvmfs/larsoft.opensciencegrid.org/products/libxml2/v2_9_5/Linux64bit+2.6-2.12-prof/lib/ -DLIBXML2_INC=/cvmfs/larsoft.opensciencegrid.org/products/libxml2/v2_9_5/Linux64bit+2.6-2.12-prof/include/libxml2 -DPYTHIA6=/cvmfs/larsoft.opensciencegrid.org/products/pythia/v6_4_28i/Linux64bit+2.6-2.12-gcc640-prof/lib
-make systematicstools # force this to build first
-make
-make install
+make -j systematicstools # force this to build first
+make -j
+make -j install
 
 cd ${TOPDIR}
 
 # Get Geometric efficiency library and build
+if [ $FORCE == yes ]; then rm -rf DUNE_ND_GeoEff; fi
 git clone --recurse-submodules https://github.com/cvilelasbu/DUNE_ND_GeoEff.git
 cd DUNE_ND_GeoEff
 cmake -DPYTHON_EXECUTABLE:FILEPATH=`which python` .
-make pyGeoEff
+make -j pyGeoEff
 
 cd ${TOPDIR}
 
