83622e49475de838be1e7aea557ef18c1528643c
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2021-09-03 14:40:45 -0400

    replace the individual branches in the main CAF tree with a StandardRecord

--- CAF.h
+++ CAF.h
@@ -4,58 +4,24 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "Ntuple/NtpMCEventRecord.h"
+#include "duneanaobj/StandardRecord/StandardRecord.h"
+#include "duneanaobj/StandardRecord/SRGlobal.h"
+#include "nusystematics/artless/response_helper.hh"
 
 class CAF {
 
 public:
-  CAF( std::string filename, bool isGas = false );
-  ~CAF();
+  CAF(  const std::string& filename, const std::string& rw_fhicl_filename );
+  ~CAF() = default;
   void fill();
   void fillPOT();
   void write();
-  void addRWbranch( int parId, std::string name, std::string wgt_var, std::vector<double> &vars );
   void Print();
   void setToBS();
 
   // Make ntuple variables public so they can be set from other file
-
-  // configuration variables
-  int isFD, isFHC;
-  // event accounting
-  int run, subrun, event;
-  // Truth information
-  int isCC, neutrinoPDG, neutrinoPDGunosc, mode, LepPDG; 
-  double Ev, Q2, W, X, Y, NuMomX, NuMomY, NuMomZ, LepMomX, LepMomY, LepMomZ, LepE, LepNuAngle;
-  // True particle counts
-  int nP, nN, nipip, nipim, nipi0, nikp, nikm, nik0, niem, niother, nNucleus, nUNKNOWN;
-  double eP, eN, ePip, ePim, ePi0, eOther;
-  double eRecoP, eRecoN, eRecoPip, eRecoPim, eRecoPi0, eRecoOther;
-
-  // vertex -- smear it?
-  double vtx_x, vtx_y, vtx_z;
-  double det_x;
-
-  // Reco information CV
-  double Ev_reco, Elep_reco, theta_reco;
-  int reco_numu, reco_nue, reco_nc, reco_q;
-  int muon_contained, muon_tracker, muon_ecal, muon_exit, reco_lepton_pdg;
-  float muon_endpoint[3];
-  std::string * muon_endVolName;
-  double Ehad_veto;
-  double pileup_energy;
-
-  // Gas TPC variables
-  int gastpc_pi_min_mult, gastpc_pi_pl_mult;
-  int nFSP;
-  int pdg[100];
-  double trkLen[100], trkLenPerp[100], ptrue[100], partEvReco[100];
-
-  // reweights -- make sure big enough to hold all the variations for each knob, and all the knobs
-  // the names, and what they actually mean, are determined automatically from the fhicl input file
-  int nwgt[100];
-  double cvwgt[100];
-  double wgt[100][100];
-  bool iswgt[100];
+  caf::StandardRecord sr;
+  caf::SRGlobal srglobal;
 
   // store the GENIE record as a branch
   genie::NtpMCEventRecord * mcrec;
@@ -69,9 +35,13 @@ public:
   int version;
 
   TFile * cafFile;
+  TTree * cafSR;
+  TTree * cafSRGlobal;
   TTree * cafMVA;
   TTree * cafPOT;
   TTree * genie;
+
+  nusyst::response_helper rh;
 };
 
 #endif
