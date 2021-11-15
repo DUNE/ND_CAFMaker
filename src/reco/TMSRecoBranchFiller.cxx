4e7341975a72e598c1db20a2bbbb389548dc2e13
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2021-11-15 13:49:03 -0600

    skeleton for filling TMS reco info

---
+++ src/reco/TMSRecoBranchFiller.cxx
@@ -0,0 +1,23 @@
+#include "TMSRecoBranchFiller.h"
+
+namespace cafmaker
+{
+  TMSRecoBranchFiller::TMSRecoBranchFiller(const std::string &tmsRecoFilename)
+    : fTMSRecoFile(tmsRecoFilename.c_str())
+  {
+    if (!fTMSRecoFile.IsZombie())
+      SetConfigured(true);
+  }
+
+  // ---------------------------------------------------------------------------
+
+  void TMSRecoBranchFiller::_FillRecoBranches(std::size_t evtIdx,
+                                              caf::StandardRecord &sr,
+                                              const cafmaker::dumpTree &dt,
+                                              const cafmaker::params &par) const
+  {
+    // here we copy all the TMS reco into the SRTMS branch of the StandardRecord object.
+
+  }
+
+}
---
+++ src/reco/TMSRecoBranchFiller.h
@@ -0,0 +1,30 @@
+/// Fill TMS reco branches from TMS reco output.
+///
+/// \author  J. Wolcott <jwolcott@fnal.gov> & F. Akbar <fakbar@ur.rochester.edu>
+/// \date    Nov. 2021
+
+#ifndef ND_CAFMAKER_TMSRECOBRANCHFILLER_H
+#define ND_CAFMAKER_TMSRECOBRANCHFILLER_H
+
+#include "IRecoBranchFiller.h"
+
+#include "TFile.h"
+
+namespace cafmaker
+{
+  class TMSRecoBranchFiller : IRecoBranchFiller
+  {
+    public:
+      TMSRecoBranchFiller(const std::string & tmsRecoFilename);
+
+    private:
+      void _FillRecoBranches(std::size_t evtIdx,
+                             caf::StandardRecord &sr,
+                             const dumpTree &dt,
+                             const params &par) const override;
+
+      TFile fTMSRecoFile;
+  };
+
+}
+#endif //ND_CAFMAKER_TMSRECOBRANCHFILLER_H
