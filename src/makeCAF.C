880282821400ebc421f85330fff0ad8240f010d3
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-01-05 17:26:02 -0500

    reorganize the reco filling as a sequence of IRecoBranchFillers
    this makes building up the list of algs to run much simpler.

--- src/makeCAF.C
+++ src/makeCAF.C
@@ -13,6 +13,8 @@
 #include "Params.h"
 #include "reco/MLNDLArRecoBranchFiller.h"
 #include "reco/ParameterizedRecoBranchFiller.h"
+#include "reco/TMSRecoBranchFiller.h"
+#include "reco/NDLArTMSMatchRecoFiller.h"
 #include "truth/FillTruth.h"
 
 
@@ -28,12 +30,44 @@ fhicl::Table<cafmaker::FhiclConfig> parseConfig(const std::string & configFile)
 }
 
 // -------------------------------------------------
+// decide which reco fillers we need based on the configuration
+std::vector<std::unique_ptr<cafmaker::IRecoBranchFiller>> getRecoFillers(const cafmaker::Params &par)
+{
+  std::vector<std::unique_ptr<cafmaker::IRecoBranchFiller>> recoFillers;
+
+  // first: did we do ND-LAr reco?
+  std::string ndlarFile;
+  if (par().cafmaker().ndlarRecoFile(ndlarFile))
+    recoFillers.emplace_back(std::make_unique<cafmaker::MLNDLArRecoBranchFiller>(ndlarFile));
+  else
+  {
+    // use the run+subrun numbers to seed the random number generator if seed is not explicitly provided
+    // (yes, leak this pointer.  there's only one and sending it back to the main() is annoying)
+    auto rando = new TRandom3(par().cafmaker().seed() >= 0 ?
+                              par().cafmaker().seed() :
+                              par().runInfo().run() * 1000 + par().runInfo().subrun());
+
+    recoFillers.emplace_back(std::make_unique<cafmaker::ParameterizedRecoBranchFiller>(rando));
+  }
+
+  // next: did we do TMS reco?
+  std::string tmsFile;
+  if (par().cafmaker().tmsRecoFile(tmsFile))
+    recoFillers.emplace_back(std::make_unique<cafmaker::TMSRecoBranchFiller>(ndlarFile));
+
+  // if we did both ND-LAr and TMS, we should try to match them, too
+  if (!ndlarFile.empty() && !tmsFile.empty())
+     recoFillers.emplace_back(std::make_unique<cafmaker::NDLArTMSMatchRecoFiller>());
+
+  return recoFillers;
+}
 
 // main loop function
-void loop(CAF& caf, cafmaker::Params &par,
+void loop(CAF& caf,
+          cafmaker::Params &par,
           TTree * intree,
           TTree * gtree,
-          const cafmaker::IRecoBranchFiller & recoFiller)
+          const std::vector<std::unique_ptr<cafmaker::IRecoBranchFiller>> & recoFillers)
 {
   //// read in dumpTree output file
   cafmaker::dumpTree dt;
@@ -58,8 +92,14 @@ void loop(CAF& caf, cafmaker::Params &par,
     caf.sr.isFD = 0;
     caf.sr.isFHC = par().runInfo().fhc();
 
+    // in the future this can be extended to use 'truth fillers'
+    // (like the reco ones) if we find that the truth filling
+    // is getting too complex for one function
     fillTruth(caf.sr, dt, gtree, caf.mcrec, par, caf.rh);
-    recoFiller.FillRecoBranches(ii, caf.sr, dt, par);
+
+    // hand off to the correct reco filler(s).
+    for (const auto & filler : recoFillers)
+      filler->FillRecoBranches(ii, caf.sr, dt, par);
 
     caf.fill();
   }
@@ -99,19 +139,7 @@ int main( int argc, char const *argv[] )
   TFile * gf = new TFile( par().cafmaker().ghepFile().c_str() );
   TTree * gtree = (TTree*) gf->Get( "gtree" );
 
-  // use the run+subrun numbers to seed the random number generator if seed is not explicitly provided
-  TRandom3 rando(par().cafmaker().seed() >= 0 ?
-                 par().cafmaker().seed() :
-                 par().runInfo().run() * 1000 + par().runInfo().subrun());
-
-  // hand off to the correct reco filler.
-  std::unique_ptr<cafmaker::IRecoBranchFiller> recoFiller(nullptr);
-  std::string ndlarFile;
-  if (par().cafmaker().ndlarRecoFile(ndlarFile))
-    recoFiller = std::make_unique<cafmaker::MLNDLArRecoBranchFiller>(ndlarFile);
-  else
-    recoFiller = std::make_unique<cafmaker::ParameterizedRecoBranchFiller>(&rando);
-  loop( caf, par, tree, gtree, *recoFiller );
+  loop( caf, par, tree, gtree, getRecoFillers(par) );
 
   caf.version = 4;
   printf( "Run %d POT %g\n", caf.meta_run, caf.pot );
