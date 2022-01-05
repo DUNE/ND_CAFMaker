6200e0c79449b469624571660258c53ac9c3a17a
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-01-05 16:31:48 -0500

    eliminate the old 'params' now that we have a replacement

--- src/Params.h
+++ src/Params.h
@@ -61,18 +61,6 @@ namespace cafmaker
   };
   using Params = fhicl::Table<cafmaker::FhiclConfig>;
 
-  // params will be extracted from command line, and passed to the reconstruction
-  struct params {
-    double OA_xcoord;
-    bool fhc, grid, IsGasTPC;
-    int seed, run, subrun, first, n, nfiles;
-    double trk_muRes, LAr_muRes, ECAL_muRes;
-    double em_const, em_sqrtE;
-    double michelEff;
-    double CC_trk_length;
-    double pileup_frac, pileup_max;
-    double gastpc_len, gastpc_B, gastpc_padPitch, gastpc_X0;
-  };
 }
 
 #endif //ND_CAFMAKER_PARAMS_H
