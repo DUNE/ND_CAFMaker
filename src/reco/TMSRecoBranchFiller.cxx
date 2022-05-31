cb22e3d07f617c706af1d64093031830fd3de577
Clarence Wret <cwret@fnal.gov>
2022-05-31 16:30:36 -0500

    Add TMS reco information to output CAF. The direction vector still needs some love, and would be good to add the physical (cm) track length to output in TMS reco

--- src/reco/TMSRecoBranchFiller.cxx
+++ src/reco/TMSRecoBranchFiller.cxx
@@ -3,21 +3,77 @@
 namespace cafmaker
 {
   TMSRecoBranchFiller::TMSRecoBranchFiller(const std::string &tmsRecoFilename)
-    : fTMSRecoFile(tmsRecoFilename.c_str())
   {
-    if (!fTMSRecoFile.IsZombie())
+    fTMSRecoFile = new TFile(tmsRecoFilename.c_str(), "READ");
+    name = std::string("TMS");
+    if (!fTMSRecoFile->IsZombie()) {
       SetConfigured(true);
+      // Save pointer to input tree
+      TMSRecoTree = dynamic_cast<TTree*>(fTMSRecoFile->Get("Line_Candidates"));
+      TMSRecoTree->SetBranchAddress("nLines", &_nLines);
+      TMSRecoTree->SetBranchAddress("nHitsInTrack", _nHitsInTrack);
+      TMSRecoTree->SetBranchAddress("TrackLength", _TrackLength);
+      TMSRecoTree->SetBranchAddress("TotalTrackEnergy", _TotalTrackEnergy);
+      TMSRecoTree->SetBranchAddress("Occupancy", _Occupancy);
+      TMSRecoTree->SetBranchAddress("DirectionX", _DirectionX);
+      TMSRecoTree->SetBranchAddress("DirectionZ", _DirectionZ);
+      TMSRecoTree->SetBranchAddress("TrackHitPos", _TrackHitPos);
+    } else {
+      fTMSRecoFile = NULL;
+      TMSRecoTree = NULL;
+    }
   }
 
   // ---------------------------------------------------------------------------
 
+  // here we copy all the TMS reco into the SRTMS branch of the StandardRecord object.
   void TMSRecoBranchFiller::_FillRecoBranches(std::size_t evtIdx,
-                                              caf::StandardRecord &sr,
-                                              const cafmaker::dumpTree &dt,
-                                              const cafmaker::Params &par) const
+      caf::StandardRecord &sr,
+      const cafmaker::dumpTree &dt,
+      const cafmaker::Params &par) const
   {
-    // here we copy all the TMS reco into the SRTMS branch of the StandardRecord object.
+    // Get nth entry from tree
+    TMSRecoTree->GetEntry(evtIdx);
+
+    // First set number of tracks
+    sr.nd.tms.ntracks = _nLines;
+    sr.nd.tms.tracks.resize(_nLines);
+
+    // Fill in the track info 
+    for (int i = 0; i < _nLines; ++i) {
+      double prevz = -9E10;
+      // Hit info
+      // Loop might be unnecessary if you really want optimisation...
+      for (int j = 0; j < _nHitsInTrack[i]; ++j) {
+        double z = _TrackHitPos[i][j][0];
+        double x = _TrackHitPos[i][j][1];
+        //std::cout << "line " << i << ", hit " << j << ", x, z = " << x << ", " << z << std::endl;
+
+        // Should all be ordered in z, check this
+        if (z < prevz) {
+          std::cerr << "hits in z not ordered" << std::endl;
+          throw;
+        }
+        prevz = z;
+      }
+
+      // Save first and last hit in track
+      sr.nd.tms.tracks[i].start = caf::SRVector3D(_TrackHitPos[i][0][0], 
+          -999, 
+          _TrackHitPos[i][0][1]);
+      sr.nd.tms.tracks[i].end = caf::SRVector3D(_TrackHitPos[i][_nHitsInTrack[i]-1][0], 
+          -999, 
+          _TrackHitPos[i][_nHitsInTrack[i]-1][1]);
+
+      // Track info
+      sr.nd.tms.tracks[i].len_gcm3  = _TrackLength[i];
+      sr.nd.tms.tracks[i].qual      = _Occupancy[i];
+      sr.nd.tms.tracks[i].E         = _TotalTrackEnergy[i];
+
+      sr.nd.tms.tracks[i].dir = caf::SRVector3D(_DirectionX[i], -999, _DirectionZ[i]);
+      // maybe want to fill this out?
+      sr.nd.tms.tracks[i].enddir = caf::SRVector3D(-999, -999, -999);
+    }
 
   }
-
-}
+} // end namespace
--- src/reco/TMSRecoBranchFiller.h
+++ src/reco/TMSRecoBranchFiller.h
@@ -6,9 +6,18 @@
 #ifndef ND_CAFMAKER_TMSRECOBRANCHFILLER_H
 #define ND_CAFMAKER_TMSRECOBRANCHFILLER_H
 
+#include <iostream>
+
+// The virtual base class
 #include "IRecoBranchFiller.h"
 
+// File handlers from ROOT
 #include "TFile.h"
+#include "TTree.h"
+
+// The duneanaobj includes
+#include "duneanaobj/StandardRecord/StandardRecord.h"
+//#include "duneanaobj/StandardRecord/SRTrack.h"
 
 namespace cafmaker
 {
@@ -16,6 +25,13 @@ namespace cafmaker
   {
     public:
       TMSRecoBranchFiller(const std::string & tmsRecoFilename);
+      ~TMSRecoBranchFiller() {
+        delete TMSRecoTree;
+        fTMSRecoFile->Close();
+        delete fTMSRecoFile;
+        TMSRecoTree = NULL;
+        fTMSRecoFile = NULL;
+      }
 
     private:
       void _FillRecoBranches(std::size_t evtIdx,
@@ -23,7 +39,20 @@ namespace cafmaker
                              const dumpTree &dt,
                              const cafmaker::Params &par) const override;
 
-      TFile fTMSRecoFile;
+      TFile *fTMSRecoFile;
+      TTree *TMSRecoTree;
+
+      // Save the branches that we're reading in
+      int _nLines;
+      // [10] should match TMS reco output
+      int _nHitsInTrack[10];
+      float _TrackLength[10];
+      float _TotalTrackEnergy[10];
+      float _Occupancy[10];
+      float _DirectionX[10];
+      float _DirectionZ[10];
+      // [10][200][2] needs to match TMS reco output (check file if in doubt)
+      float _TrackHitPos[10][200][2];
   };
 
 }
