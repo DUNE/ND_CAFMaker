9ce0f7c15359ec79ad80abad471c6dd013f5f2ce
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2022-08-17 09:37:07 -0500

    rework of SAND reco filler

--- src/reco/SANDRecoBranchFiller.cxx
+++ src/reco/SANDRecoBranchFiller.cxx
@@ -1,66 +1,73 @@
 /// Fill reco branches using SAND reco data
 ///
+/// \author  L. Di Noto, reworked by M. Vicenzi
+/// \date    Apr. 2022
+///
 
 #include "SANDRecoBranchFiller.h"
 
-#include "TF1.h"
-#include "TRandom.h"
-#include "TVector3.h"
-#include "TLorentzVector.h"
-#include "TFile.h"
-#include "TTree.h"
-#include <TSystem.h>
-
 #include "duneanaobj/StandardRecord/StandardRecord.h"
 
-#include "Params.h"
+#include "TSystem.h"
 #include "struct.h"    
-#include "SANDEvt.h"
 
 namespace cafmaker
 {
-  const double mmu = 0.1056583745;
-  void SANDRecoBranchFiller::_FillRecoBranches(std::size_t ii, //elemento del tree da guardare.. 
-                                                        caf::StandardRecord &sr,
-                                                        const cafmaker::Params &par) const
+  
+  SANDRecoBranchFiller::SANDRecoBranchFiller(const std::string &SANDRecoFilename){
+    
+    gSystem->Load("/dune/app/users/mvicenzi/FastReco/lib/libStruct.so");
+    
+    fSANDRecoFile = new TFile(SANDRecoFilename.c_str());
+    fTree = (TTree*) fSANDRecoFile->Get("tEvent");
+
+    fEvent = new event;
+    fTree->SetBranchAddress("event", &fEvent);
+
+    if(!fSANDRecoFile->IsZombie())
+      SetConfigured(true);
+  }
+
+  void SANDRecoBranchFiller::_FillRecoBranches(std::size_t ii, 
+					       caf::StandardRecord &sr,
+					       const cafmaker::Params &par) const
   {
-   //std::cout<<"riempio con i dati di SAND!"<<std::endl;
-   //devo aprire sand-reco da cui prendere i dati corrispondenti all'elemento size_t del tree
-    std::string sandFile;
-    par().cafmaker().sandRecoFile(sandFile);
 
-    particle Recolepton;
-    if(SANDEvt::Get()->GetSANDEvtLepton(Recolepton)){
-	   sr.reco_lepton_pdg = Recolepton.pdg;
- 	   sr.Elep_reco =Recolepton.Ereco*0.001;  //in MeV
-    if(Recolepton.pdg==13){ sr.reco_numu = 1; sr.reco_nue=0; sr.reco_nc=0;}
-    if(Recolepton.pdg==11){  sr.reco_numu = 0; sr.reco_nue=1; sr.reco_nc=0;}
+   fTree->GetEntry(ii);
+    
+   //todo: currently filling simple variables
+   //rewrite once sr.nd.sand exists in StandardRecord
 
-  }else{
-//Lepton not found in reco event --> it is assumed a nc event
-	 sr.reco_numu = 0; sr.reco_nue=0; sr.reco_nc=1;
-	 sr.reco_lepton_pdg=-1;	
-    	 sr.Elep_reco=-1;	
+   // neutrino energy
+   sr.Ev_reco = fEvent->Enureco*0.001; //GeV
+  
+   std::vector<particle> particle_event = fEvent->particles; 
+   bool foundLepton = false;
+   for ( auto it = particle_event.begin(); it != particle_event.end(); ++it){
 
-	}
- 
-    //neutrino energy
-    sr.Ev_reco = SANDEvt::Get()->GetSANDEvtEnureco()*0.001 ; 
+      // primary lepton
+      if( abs(((*it).pdg) == 13 || abs((*it).pdg) == 11) && (*it).primary == 1){
+        sr.reco_lepton_pdg = (*it).pdg;
+	sr.Elep_reco = (*it).Ereco*0.001;  //GeV
+        foundLepton = true;
+      }
 
-    //reco energy by species
-    sr.eRecoP = SANDEvt::Get()->GetSANDEvtErecopdg(2212)*0.001;   
-    sr.eRecoN = SANDEvt::Get()->GetSANDEvtErecopdg(2112)*0.001;   
-    sr.eRecoPip = SANDEvt::Get()->GetSANDEvtErecopdg(211)*0.001;   
-    sr.eRecoPim = SANDEvt::Get()->GetSANDEvtErecopdg(-211)*0.001;   
-    sr.eRecoPi0 = SANDEvt::Get()->GetSANDEvtErecopdg(111)*0.001;   
-    sr.eRecoOther = SANDEvt::Get()->GetSANDEvtErecopdg(0)*0.001;
+      // other species
+      // ...
+	
+   }
+    
+   // event flags
+   if(!foundLepton){ //flags as nc
+     sr.reco_numu = 0; sr.reco_nue=0; sr.reco_nc=1;
+     sr.reco_lepton_pdg = -1;	
+     sr.Elep_reco = -1;	
+   }else if(sr.reco_lepton_pdg == 13){ //numu
+     sr.reco_numu = 1; sr.reco_nue = 0; sr.reco_nc = 0; 
+   }else if(sr.reco_lepton_pdg == 11){ //nue
+     sr.reco_numu = 0; sr.reco_nue = 1; sr.reco_nc = 0;
+   }
 
-/*
-   sr.reco_q = 100;
-   sr.muon_contained = 100; 
-   sr.muon_tracker = 100; 
-   sr.muon_ecal = 100; 
-   sr.muon_exit = 1;
-*/
- }
+
+  }
 }
--- src/reco/SANDRecoBranchFiller.h
+++ src/reco/SANDRecoBranchFiller.h
@@ -7,38 +7,27 @@
 
 #include "IRecoBranchFiller.h"
 
-class TLorentzVector;
-class TRandom;
-class TVector3;
+#include "TFile.h"
+#include "TTree.h"
+#include "struct.h" //FIXME
 
 namespace cafmaker
 {
 
-  class SANDRecoBranchFiller : public IRecoBranchFiller
+  class SANDRecoBranchFiller : public cafmaker::IRecoBranchFiller
   {
     public:
-      explicit SANDRecoBranchFiller(const std::string &sand_reco_file)
-        : fSandfile(sand_reco_file)
-      {
-        // we don't have any further files to load or anything,
-        // so we're all set
-        SetConfigured(true);
-      }
+      SANDRecoBranchFiller(const std::string &SANDRecoFilename);
 
-    protected:
+
+    private:
       void _FillRecoBranches(std::size_t evtIdx,
 			     caf::StandardRecord &sr,
 			     const cafmaker::Params &par) const override;
 
-    private:
-  //    void decayPi0( const TLorentzVector & pi0, TVector3 &gamma1, TVector3 &gamma2 ) const;
-
-    //  void recoElectron(caf::StandardRecord & sr, const cafmaker::Params &par ) const;
-    //  void recoMuonECAL(caf::StandardRecord & sr, const cafmaker::Params &par ) const;
-    //  void recoMuonLAr(caf::StandardRecord & sr, const cafmaker::Params &par ) const;
-    //  void recoMuonTracker(caf::StandardRecord & sr, const cafmaker::Params &par ) const;
-
-      std::string fSandfile;
+      TFile* fSANDRecoFile;
+      TTree* fTree;
+      struct event* fEvent;
   };
 
 }
