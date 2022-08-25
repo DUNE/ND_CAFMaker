9e22a8af5930fb61c731636076af59c90cab870a
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-08-25 12:29:04 -0400

    re-fix some of the dependency updates that got overwritten in merge w/ master

--- ndcaf_setup.sh
+++ ndcaf_setup.sh
@@ -14,7 +14,7 @@ setup hdf5 v1_10_5a -q e20
 setup fhiclcpp v4_15_03 -q debug:e20
 
 # edep-sim needs to know where a certain GEANT .cmake file is...
-G4_cmake_file=`find ${GEANT4_FQ_DIR}/lib64 -name 'Geant4Config.cmake'`
+G4_cmake_file=`find ${GEANT4_FQ_DIR}/lib -name 'Geant4Config.cmake'`
 export Geant4_DIR=`dirname $G4_cmake_file`
 
 # edep-sim needs to have the GEANT bin directory in the path
