b31b424f5c5941d984eceb09c62e81c66f74f263
Francisco Martinez Lopez <fmlopez@dunegpvm07.fnal.gov>
2023-01-25 11:07:23 -0600

    Add new track and ECAL info in NDGAr reco branch filler

--- src/reco/NDGArRecoBranchFiller.cxx
+++ src/reco/NDGArRecoBranchFiller.cxx
@@ -25,10 +25,50 @@ namespace cafmaker
       NDGArRecoTree = dynamic_cast<TTree*>(fNDGArRecoFile->Get("anatree/GArAnaTree"));
 
       NDGArRecoTree->SetBranchAddress("Event", &fEvent);
+
+      //Track-related info
       NDGArRecoTree->SetBranchAddress("TrackStartX", &fTrackStartX);
-      NDGArRecoTree->SetBranchAddress("TrackPIDCheatedF", &fTrackPIDCheatedF);
+      NDGArRecoTree->SetBranchAddress("TrackStartY", &fTrackStartY);
+      NDGArRecoTree->SetBranchAddress("TrackStartZ", &fTrackStartZ);
+      NDGArRecoTree->SetBranchAddress("TrackEndX", &fTrackEndX);
+      NDGArRecoTree->SetBranchAddress("TrackEndY", &fTrackEndY);
+      NDGArRecoTree->SetBranchAddress("TrackEndZ", &fTrackEndZ);
+
+      NDGArRecoTree->SetBranchAddress("TrackStartPx", &fTrackStartPx);
+      NDGArRecoTree->SetBranchAddress("TrackStartPy", &fTrackStartPy);
+      NDGArRecoTree->SetBranchAddress("TrackStartPz", &fTrackStartPz);
+      NDGArRecoTree->SetBranchAddress("TrackEndPx", &fTrackEndPx);
+      NDGArRecoTree->SetBranchAddress("TrackEndPy", &fTrackEndPy);
+      NDGArRecoTree->SetBranchAddress("TrackEndPz", &fTrackEndPz);
+
+      NDGArRecoTree->SetBranchAddress("TrackIDNumber", &fTrackIDNumber);
+
       NDGArRecoTree->SetBranchAddress("TrackLenF", &fTrackLenF);
+      NDGArRecoTree->SetBranchAddress("TrackLenB", &fTrackLenB);
       NDGArRecoTree->SetBranchAddress("TrackPF", &fTrackPF);
+      NDGArRecoTree->SetBranchAddress("TrackPB", &fTrackPB);
+      NDGArRecoTree->SetBranchAddress("TrackAvgIonF", &fTrackAvgIonF);
+      NDGArRecoTree->SetBranchAddress("TrackAvgIonB", &fTrackAvgIonB);
+
+      NDGArRecoTree->SetBranchAddress("TrackPIDF", &fTrackPIDF);
+      NDGArRecoTree->SetBranchAddress("TrackPIDProbF", &fTrackPIDProbF);
+      NDGArRecoTree->SetBranchAddress("TrackPIDB", &fTrackPIDB);
+      NDGArRecoTree->SetBranchAddress("TrackPIDProbB", &fTrackPIDProbB);
+
+      //ECAL-related info
+      NDGArRecoTree->SetBranchAddress("ClusterX", &fECALClusterX);
+      NDGArRecoTree->SetBranchAddress("ClusterY", &fECALClusterY);
+      NDGArRecoTree->SetBranchAddress("ClusterZ", &fECALClusterZ);
+
+      NDGArRecoTree->SetBranchAddress("ClusterIDNumber", &fECALClusterIDNumber);
+
+      NDGArRecoTree->SetBranchAddress("ClusterEnergy", &fECALClusterEnergy);
+      NDGArRecoTree->SetBranchAddress("ClusterNhits", &fECALClusterNhits);
+
+      //ECAL-track associations
+      NDGArRecoTree->SetBranchAddress("ECALAssn_ClusIDNumber", &fECALAssn_ClusterID);
+      NDGArRecoTree->SetBranchAddress("ECALAssn_TrackIDNumber", &fECALAssn_TrackID);
+
     } else {
       NDGArRecoTree = NULL;
       std::cerr << "Did not find input ND-GAr reco file you provided: " << NDGArRecoFilename << std::endl;
@@ -43,9 +83,70 @@ namespace cafmaker
   {
    
    NDGArRecoTree->GetEntry(ii);
-
    std::cout << "Event number: " << fEvent << std::endl;
-   std::cout << "Number of particles: " << fTrackStartX->size() << std::endl;
+   std::cout << "Number of reco tracks: " << fTrackStartX->size() << std::endl;
+   std::cout << "Number of reco ECAL clusters: " << fECALClusterX->size() << std::endl;
+
+   int n_tracks = fTrackStartX->size();
+   sr.nd.gar.ntracks = n_tracks;
+   for (size_t i=0; i<n_tracks; i++){
+      caf::SRGArTrack track;
+
+      caf::SRVector3D start(fTrackStartX->at(i), fTrackStartY->at(i), fTrackStartZ->at(i));
+      track.start = start;
+      caf::SRVector3D end(fTrackEndX->at(i), fTrackEndY->at(i), fTrackEndZ->at(i));
+      track.end = end;
+      caf::SRVector3D dir(fTrackStartPx->at(i), fTrackStartPy->at(i), fTrackStartPz->at(i));
+      track.start = dir;
+      caf::SRVector3D enddir(fTrackEndPx->at(i), fTrackEndPy->at(i), fTrackEndPz->at(i));
+      track.start = enddir;
+
+      track.len_cm_F = fTrackLenF->at(i);
+      track.len_cm_B = fTrackLenB->at(i);
+      track.p_F = fTrackPF->at(i);
+      track.p_B = fTrackPB->at(i);
+      track.dEdx_F = fTrackAvgIonF->at(i);
+      track.dEdx_B = fTrackAvgIonB->at(i);
+
+      std::vector<int> pid_F = fTrackPIDF->at(i);
+      track.pid_F = pid_F;
+      std::vector<float_t> pid_prob_F = fTrackPIDProbF->at(i);
+      track.pid_prob_F = pid_prob_F;
+      std::vector<int> pid_B = fTrackPIDB->at(i);
+      track.pid_F = pid_B;
+      std::vector<float_t> pid_prob_B = fTrackPIDProbB->at(i);
+      track.pid_prob_B = pid_prob_B;
+
+      sr.nd.gar.tracks.push_back(track);
+   }
+
+   int n_clusters = fECALClusterX->size();
+   sr.nd.gar.nclusters = n_clusters;
+
+   int n_assns = fECALAssn_ClusterID->size();
+   
+   for (size_t i=0; i<n_clusters; i++){
+      caf::SRGArECAL cluster;
+
+      caf::SRVector3D position(fECALClusterX->at(i), fECALClusterY->at(i), fECALClusterZ->at(i));
+      cluster.position = position;
+
+      cluster.E = fECALClusterEnergy->at(i);
+      cluster.hits_in_cluster = fECALClusterNhits->at(i);
+
+      cluster.ecal_id = fECALClusterIDNumber->at(i);
+
+      for (size_t j=0; j<n_assns; ++j){
+          if (cluster.ecal_id == fECALAssn_ClusterID->at(j)){
+              cluster.trk_assn = fECALAssn_TrackID->at(j);
+          }
+      }
+
+   }
+
+   //legacy variables
+   //std::cout << "Event number: " << fEvent << std::endl;
+   //std::cout << "Number of particles: " << fTrackStartX->size() << std::endl;
     
    //todo: currently filling pseudo reco variables in SRGAr.h
    //once GAr reco is integrated they should be replaced with proper reco
--- src/reco/NDGArRecoBranchFiller.h
+++ src/reco/NDGArRecoBranchFiller.h
@@ -31,10 +31,47 @@ namespace cafmaker
       TTree* NDGArRecoTree;
       
       int                fEvent;
+
       std::vector<float_t> * fTrackStartX;
-      std::vector<int>   * fTrackPIDCheatedF;
+      std::vector<float_t> * fTrackStartY;
+      std::vector<float_t> * fTrackStartZ;
+      std::vector<float_t> * fTrackEndX;
+      std::vector<float_t> * fTrackEndY;
+      std::vector<float_t> * fTrackEndZ;
+
+      std::vector<float_t> * fTrackStartPx;
+      std::vector<float_t> * fTrackStartPy;
+      std::vector<float_t> * fTrackStartPz;
+      std::vector<float_t> * fTrackEndPx;
+      std::vector<float_t> * fTrackEndPy;
+      std::vector<float_t> * fTrackEndPz;
+
       std::vector<float_t> * fTrackLenF;
+      std::vector<float_t> * fTrackLenB;
       std::vector<float_t> * fTrackPF;
+      std::vector<float_t> * fTrackPB;
+      std::vector<float_t> * fTrackAvgIonF;
+      std::vector<float_t> * fTrackAvgIonB;
+
+      std::vector<int> * fTrackIDNumber;
+
+      std::vector<std::vector<int>>     * fTrackPIDF;
+      std::vector<std::vector<float_t>> * fTrackPIDProbF;
+      std::vector<std::vector<int>>     * fTrackPIDB;
+      std::vector<std::vector<float_t>> * fTrackPIDProbB;
+
+      std::vector<float_t> * fECALClusterX;
+      std::vector<float_t> * fECALClusterY;
+      std::vector<float_t> * fECALClusterZ;
+
+      std::vector<int> * fECALClusterIDNumber;
+
+      std::vector<float_t> * fECALClusterEnergy;
+      std::vector<int>     * fECALClusterNhits;
+
+      std::vector<int> * fECALAssn_ClusterID;
+      std::vector<int> * fECALAssn_TrackID;
+    
   };
 
 }
