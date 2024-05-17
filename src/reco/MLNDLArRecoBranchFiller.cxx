d1e3c1a1db2653178ef73baad03cf65b7ba705eb
Sindhujha Kumaran <49272937+sindhu-ku@users.noreply.github.com>
2024-05-17 10:36:47 -0700

    remove std::distance
    Co-authored-by: Jeremy Wolcott <chenel@users.noreply.github.com>

--- src/reco/MLNDLArRecoBranchFiller.cxx
+++ src/reco/MLNDLArRecoBranchFiller.cxx
@@ -638,8 +638,8 @@ namespace cafmaker
                                            std::find_if(sr.mc.nu.begin(),
                                                         sr.mc.nu.end(),
                                                         [&srTrueInt](const caf::SRTrueInteraction& ixn) {return ixn.id == srTrueInt.id;}));
-	  std::size_t truthPartIdx = std::distance(srTrueInt.prim.begin(), std::find_if(srTrueInt.prim.begin(), srTrueInt.prim.end(), [&srTrueInt, &truePartPassThrough](const caf::SRTrueParticle& part) { return part.G4ID == truePartPassThrough.gen_id; }));
-          bool is_primary = truthPartIdx != srTrueInt.prim.size();
+    bool is_primary = std::find_if(srTrueInt.prim.begin(), srTrueInt.prim.end(), 
+                                   [&srTrueInt, &truePartPassThrough](const caf::SRTrueParticle& part) { return part.G4ID == truePartPassThrough.gen_id; }) == srTrueInt.prim.end();
 
           srPartCmp.trkid = is_primary
                             ? truePartPassThrough.gen_id
