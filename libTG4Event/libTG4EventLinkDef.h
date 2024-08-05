6be8370d245b697577a7cce613e4dbd0813563ac
noeroy <noe.roylamoureux@gmail.com>
2024-08-04 22:41:28 -0400

    Added TG4 libraries

---
+++ libTG4Event/libTG4EventLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+
+#pragma link C++ class TG4Event+;
+#pragma link C++ class pair<string,vector<TG4HitSegment> >+;
+#pragma link C++ class TG4PrimaryVertex+;
+#pragma link C++ class TG4Trajectory+;
+#pragma link C++ class TG4PrimaryParticle+;
+#pragma link C++ class TG4TrajectoryPoint+;
+#pragma link C++ class TG4HitSegment+;
+#endif
