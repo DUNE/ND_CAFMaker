09cf1872898febdb583c79dd7317b19a6d4e1859
noeroy <noe.roylamoureux@gmail.com>
2024-08-29 23:13:15 -0500

    removed libTG4Event as we can just setup edepsim via ups

--- libTG4Event/libTG4EventProjectHeaders.h
+++
@@ -1,7 +0,0 @@
-#include "TG4Event.h"
-#include "TG4PrimaryVertex.h"
-#include "TG4Trajectory.h"
-#include "TG4PrimaryParticle.h"
-#include "TG4TrajectoryPoint.h"
-#include "TG4HitSegment.h"
-#include "libTG4EventProjectInstances.h"
