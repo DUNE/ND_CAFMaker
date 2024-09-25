4242f598a8955ab93b6b8eb7744f6e725cb565c1
Liam O'Sullivan <liam.osullivan@uni-mainz.de>
2024-09-25 09:06:03 -0500

    Committing partially working TMS CAFMaker integration

--- src/reco/TMSRecoBranchFiller.cxx
+++ src/reco/TMSRecoBranchFiller.cxx
@@ -47,8 +47,9 @@ namespace cafmaker
       TMSRecoTree->SetBranchAddress("KalmanPos",             _TrackHitPos); // TODO: how get hits from da vectur??
       //TMSRecoTree->SetBranchAddress("HitPos",             _TrackKalmanPos); // TODO: how get hits from da vectur??
       TMSRecoTree->SetBranchAddress("EndPos",                _TrackEndPos);
-      TMSRecoTree->SetBranchAddress("StartDirection",        _TrackStartDirection);
+      //TMSRecoTree->SetBranchAddress("StartDirection",        _TrackStartDirection);
       TMSRecoTree->SetBranchAddress("EndDirection",          _TrackEndDirection);
+      TMSRecoTree->SetBranchAddress("Direction",          _TrackStartDirection);
     } else {
       fTMSRecoFile = NULL;
       TMSRecoTree  = NULL;
@@ -75,71 +76,120 @@ namespace cafmaker
                                               const cafmaker::Params &par,
                                               const TruthMatcher *truthMatcher) const
   {
-#ifndef DISABLE_TMS
 
     sr.meta.tms.enabled = true;
 
     // Nicked from the MINVERvA example:
     // figure out where in our list of triggers this event index is.
     // we should always be looking forwards, since we expect to be traversing in that direction
-//    auto it_start = (fLastTriggerReqd == fTriggers.end()) ? fTriggers.cbegin() : fLastTriggerReqd;
-//    auto itTrig = std::find(it_start, fTriggers.cend(), trigger);
-//    if (itTrig == fTriggers.end())
-//    {
-//      LOG.FATAL() << "Reco branch filler '" << GetName() << "' could not find trigger with evtID == " << trigger.evtID << "!  Abort.\n";
-//      abort();
-//    }
-//    std::size_t idx = std::distance(fTriggers.cbegin(), itTrig);
-//    LOG.VERBOSE() << "    Reco branch filler '" << GetName() << "', trigger.evtID == " << trigger.evtID << ", internal evt idx = " << idx << ".\n";
+    auto it_start = (fLastTriggerReqd == fTriggers.end()) ? fTriggers.cbegin() : fLastTriggerReqd;
+    auto itTrig = std::find(it_start, fTriggers.cend(), trigger);
+    if (itTrig == fTriggers.end())
+    {
+      LOG.FATAL() << "Reco branch filler '" << GetName() << "' could not find trigger with evtID == " << trigger.evtID << "!  Abort.\n";
+      abort();
+    }
+    std::size_t idx = std::distance(fTriggers.cbegin(), itTrig);
+    LOG.VERBOSE() << "    Reco branch filler '" << GetName() << "', trigger.evtID == " << trigger.evtID << ", internal evt idx = " << idx << ".\n";
 
-
+    int i = trigger.evtID; // pseudo-itterator for ixn
     // Get nth entry from tree
-    TMSRecoTree->GetEntry(trigger.evtID);
 
-    // First set number of tracks
-    sr.nd.tms.nixn = _nTracks;
+    int LastSpillNo = -999999; //_SpillNo;
+    TMSRecoTree->GetEntry(i);
+    LastSpillNo = _SpillNo;
+    //TMSRecoTree->GetEntry(idx);//trigger.evtID);
+
+    //caf::SRTMSInt* interaction;
+    //interaction = new caf::SRTMSInt();
+
+    sr.nd.tms.ixn.emplace_back();
+    caf::SRTMSInt& interaction = sr.nd.tms.ixn.back();
+
+    interaction.ntracks = 0;
+    while (_nTracks <=1 && _SpillNo == LastSpillNo && i < TMSRecoTree->GetEntries())
+    {
+      TMSRecoTree->GetEntry(++i);
+      interaction.tracks.resize(_nTracks + interaction.tracks.size());
+      for (int j = 0; j < _nTracks; ++j) {
+        interaction.ntracks++;
+        interaction.tracks[j].start   = caf::SRVector3D(_TrackStartPos[j][0]/10., _TrackStartPos[j][1]/10., _TrackStartPos[j][2]/10.);;
+        interaction.tracks[j].end     = caf::SRVector3D(_TrackEndPos[j][0]/10., _TrackEndPos[j][1]/10., _TrackEndPos[j][2]/10.);
+        interaction.tracks[j].dir     = caf::SRVector3D(_TrackStartDirection[j][0], _TrackStartDirection[j][1] , _TrackStartDirection[j][2]);
+        interaction.tracks[j].enddir  = caf::SRVector3D(_TrackEndDirection[j][0], _TrackEndDirection[j][1] , _TrackEndDirection[j][2]);
+
+        // Calculate length by summing up the distances from the kalman reco positions
+        double tmpLength_cm = 0.0;
+        for (int k=0; k<_nHitsInTrack[j]-1; k++)
+          tmpLength_cm += sqrt( pow(_TrackHitPos[j][k][0] - _TrackHitPos[j][k+1][0], 2)
+                              + pow(_TrackHitPos[j][k][1] - _TrackHitPos[j][k+1][1], 2)
+                              + pow(_TrackHitPos[j][k][2] - _TrackHitPos[j][k+1][2], 2) );
+
+        // Track info
+        interaction.tracks[j].len_cm    = tmpLength_cm; //trackVec->Mag();
+        interaction.tracks[j].len_gcm2  = (_TrackLength[j]>0.0) ? _TrackLength[j]/10. : 0.0; // idk why we have negatives
+        interaction.tracks[j].qual      = _Occupancy[j]; // TODO: Apparently this is a "track quality", nominally (hits in track)/(total hits)
+        interaction.tracks[j].Evis      = _TrackEnergyDeposit[j];
+      }
+    }
+
+
+
+
+    /*
+    //sr.meta.tms.run = ev_run;
+    //sr.meta.tms.subrun = ev_sub_run;
+    //sr.meta.tms.event = ev_gate;
+    sr.meta.tms.readoutstart_s = trigger.triggerTime_s;
+    sr.meta.tms.readoutstart_ns = trigger.triggerTime_ns;
+    //FillInteractions(truthMatcher, sr);
+
+    //std::cout << "Trigger: " << trigger.evtID << "\t" << trigger.triggerType << "\t" << trigger.triggerTime_s << " " << trigger.triggerTime_ns << std::endl;
+
+    sr.nd.tms.nixn = fTriggers.size(); //_nTracks;
     sr.nd.tms.ixn.resize(sr.nd.tms.nixn);
-
-    //sr.nd.tms.nixn = _nLines;
-    //sr.SRTMSInt.ntracks= _nLines;
+    //sr.nd.tms.ixn.reserve(3000);//TMSRecoTree->GetEntries());
 
     // Fill in the track info 
-    for (int i = 0; i < _nTracks; ++i) {
-      /* Currently we only really care about catching (anti-)muons from LAr,
-       * so we assume that each track is probably a(n) (anti-)muon originating
-       * from a single interaction upstream in LAr.
-       *     Liam     */
+    //if (_nTracks > 0) std::cout << "idx: " << idx << "\ttracks: " << _nTracks << std::endl;
+    std::cout << "idx: " << idx << "\ttracks: " << _nTracks << std::endl;
 
-      sr.nd.tms.ixn[i].ntracks = 1; // One reco track per interaction (ixn) TODO: allow tracks to have relation in future
-      sr.nd.tms.ixn[i].tracks.resize(sr.nd.tms.ixn[i].ntracks);
+
+    std::cout << "loop ";
+    for (int j = 0; j < _nTracks; ++j) {
+      std::cout << j << " ";
+
+      sr.nd.tms.ixn[i].ntracks = _nTracks; // One reco track per interaction (ixn) TODO: allow tracks to have relation in future
+      //sr.nd.tms.ixn[i].tracks.resize(_nTracks);//sr.nd.tms.ixn[i].ntracks);
 
       // Save first and last hit in track
       // TMS Reco info is saved in mm whereas CAFs use CM as default -> do conversion here
-      sr.nd.tms.ixn[i].tracks[0].start = caf::SRVector3D(_TrackStartPos[i][0]/10., _TrackStartPos[i][1]/10., _TrackStartPos[i][2]/10.);
-      sr.nd.tms.ixn[i].tracks[0].end   = caf::SRVector3D(_TrackEndPos[i][0]/10., _TrackEndPos[i][1]/10., _TrackEndPos[i][2]/10.);
+      sr.nd.tms.ixn[i].tracks[j].start = caf::SRVector3D(_TrackStartPos[j][0]/10., _TrackStartPos[j][1]/10., _TrackStartPos[j][2]/10.);
+      sr.nd.tms.ixn[i].tracks[j].end   = caf::SRVector3D(_TrackEndPos[j][0]/10., _TrackEndPos[j][1]/10., _TrackEndPos[j][2]/10.);
       // Get the directions
       // TODO: At present tracks are completely straight objects, so dir is the same here for both
-      sr.nd.tms.ixn[i].tracks[0].dir     = caf::SRVector3D(_TrackStartDirection[i][0], _TrackStartDirection[i][1] , _TrackStartDirection[i][2]);
-      sr.nd.tms.ixn[i].tracks[0].enddir  = caf::SRVector3D(_TrackEndDirection[i][0], _TrackEndDirection[i][1] , _TrackEndDirection[i][2]);
+      sr.nd.tms.ixn[i].tracks[j].dir     = caf::SRVector3D(_TrackStartDirection[j][0], _TrackStartDirection[j][1] , _TrackStartDirection[j][2]);
+      sr.nd.tms.ixn[i].tracks[j].enddir  = caf::SRVector3D(_TrackEndDirection[j][0], _TrackEndDirection[j][1] , _TrackEndDirection[j][2]);
 
-      //TVector3* trackVec = new TVector3( (sr.nd.tms.ixn[i].tracks[0].end - sr.nd.tms.ixn[i].tracks[0].start) );
+      //TVector3* trackVec = new TVector3( (sr.nd.tms.ixn[j].tracks[0].end - sr.nd.tms.ixn[j].tracks[0].start) );
 
       // Calculate length by summing up the distances from the kalman reco positions
       double tmpLength_cm = 0.0;
-      for (int k=0; k<_nHitsInTrack[i]-1; k++)
-        tmpLength_cm += sqrt( pow(_TrackHitPos[i][k][0] - _TrackHitPos[i][k+1][0], 2)
-                            + pow(_TrackHitPos[i][k][1] - _TrackHitPos[i][k+1][1], 2)
-                            + pow(_TrackHitPos[i][k][2] - _TrackHitPos[i][k+1][2], 2) );
+      for (int k=0; k<_nHitsInTrack[j]-1; k++)
+        tmpLength_cm += sqrt( pow(_TrackHitPos[j][k][0] - _TrackHitPos[j][k+1][0], 2)
+                            + pow(_TrackHitPos[j][k][1] - _TrackHitPos[j][k+1][1], 2)
+                            + pow(_TrackHitPos[j][k][2] - _TrackHitPos[j][k+1][2], 2) );
 
       // Track info
-      sr.nd.tms.ixn[i].tracks[0].len_cm    = tmpLength_cm; //trackVec->Mag();
-      sr.nd.tms.ixn[i].tracks[0].len_gcm2  = (_TrackLength[i]>0.0) ? _TrackLength[i]/10. : 0.0; // idk why we have negatives
-      sr.nd.tms.ixn[i].tracks[0].qual      = _Occupancy[i]; // TODO: Apparently this is a "track quality", nominally (hits in track)/(total hits)
-      sr.nd.tms.ixn[i].tracks[0].Evis      = _TrackEnergyDeposit[i];
-      sr.nd.tms.ixn[i].tracks[0].E         = _TrackTotalEnergy[i];
+      sr.nd.tms.ixn[i].tracks[j].len_cm    = tmpLength_cm; //trackVec->Mag();
+      sr.nd.tms.ixn[i].tracks[j].len_gcm2  = (_TrackLength[j]>0.0) ? _TrackLength[j]/10. : 0.0; // idk why we have negatives
+      sr.nd.tms.ixn[i].tracks[j].qual      = _Occupancy[j]; // TODO: Apparently this is a "track quality", nominally (hits in track)/(total hits)
+      sr.nd.tms.ixn[i].tracks[j].Evis      = _TrackEnergyDeposit[j];
+      sr.nd.tms.ixn[i].tracks[j].E         = _TrackTotalEnergy[j];
 
       //delete trackVec;
     }
+         */
   }
 
 
@@ -147,6 +197,8 @@ namespace cafmaker
   std::deque<Trigger> TMSRecoBranchFiller::GetTriggers(int triggerType) const
   {
     std::deque<Trigger> triggers;
+    int lastSpillNo = -99999999;
+
     if (fTriggers.empty())
     {
       LOG.DEBUG() << "Loading triggers with type " << triggerType << " within branch filler '" << GetName() << "' from " << TMSRecoTree->GetEntries() << " TMS Reco_Tree:\n";
@@ -154,15 +206,21 @@ namespace cafmaker
 
       for (int entry = 0; entry < TMSRecoTree->GetEntries(); entry++)
       {
-        // TODO: BIIIIIIG TODO
-
         TMSRecoTree->GetEntry(entry);
+
+        if (_SpillNo == lastSpillNo)
+        {
+          continue; // Only first 'event' in each spill populates a trigger
+        }
+        lastSpillNo = _SpillNo;
 
         fTriggers.emplace_back();
         Trigger & trig = fTriggers.back();
-
-        trig.evtID = Long_t(_EventNo);
-
+        trig.evtID = entry; //trigger.id;
+        //trig.triggerType = trigger.type;
+        //trig.triggerTime_s = .time_s;
+        //trig.triggerTime_ns = .time_ns;
+        //trig.evtID = Long_t(_EventNo);
 
         // todo: these are placeholder values until we can propagate enough info through the reco files
         LOG.VERBOSE() << "  added trigger:  evtID=" << trig.evtID
@@ -175,12 +233,12 @@ namespace cafmaker
     for (const Trigger & trigger : fTriggers)
     {
       if (triggerType < 0 || triggerType == fTriggers.back().triggerType)
+      {
         triggers.push_back(trigger);
+      }
     }
 
     return triggers;
   }
-
-#endif  // DISABLE_TMS
 
 } // end namespace
