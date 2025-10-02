2734a70da4c5a4962026fe17ce70c49b6be797c9
Katherine Hildebrandt <khildebr@dunegpvm03.fnal.gov>
2025-10-02 12:09:51 -0500

    Added angular checks and handling for match multiplicity

--- src/reco/NDLArTMSMatchRecoFiller.cxx
+++ src/reco/NDLArTMSMatchRecoFiller.cxx
@@ -9,40 +9,84 @@ namespace cafmaker
     SetConfigured(true);
   }
 
-  bool NDLArTMSMatchRecoFiller::Spatial_Match(caf::SRTrack track_lar, caf::SRTrack track_tms, double &xthreshold, double &ythreshold) const
+  // We need a comparator to go through the std::map and there's no default comparator for SRNDLar.
+  struct LarIdComparator
   {
-        double z2_lar = track_lar.end.z;     //cm
+    bool operator()(const caf::SRNDLArID &lhs, const caf::SRNDLArID &rhs) const
+    {
+      // Define your custom comparison logic here
+      // In this example, compare based on 'id' only
+      if (lhs.ixn < rhs.ixn)
+        return true;
+      else
+      {
+        if (lhs.ixn == rhs.ixn)
+          return lhs.idx < rhs.idx;
+      }
+      return false;
+    }
+  };
 
-        // Check LAr track exits through the end; if not move to next track
-        if (z2_lar < 800){
-          return false;
-        }
+  double NDLArTMSMatchRecoFiller::Angular_Match(double xdir1_tms, double ydir1_tms, double zdir1_tms, double xdir2_lar, double ydir2_lar, double zdir2_lar) const
+  {
+      double dir_dot = (xdir1_tms * xdir2_lar) + (ydir1_tms * ydir2_lar) + (zdir1_tms * zdir2_lar);
+      double abs_tms = sqrt((xdir1_tms * xdir1_tms) + (ydir1_tms * ydir1_tms) + (zdir1_tms * zdir1_tms));
+      double abs_lar = sqrt((xdir2_lar * xdir2_lar) + (ydir2_lar * ydir2_lar) + (zdir2_lar * zdir2_lar));
 
-        // Now get the other variables
-        double x2_lar = track_lar.end.x;     //cm
-        double y2_lar = track_lar.end.y;     //cm
-        double xdir2_lar = track_lar.enddir.x; //dir cosines
-        double ydir2_lar = track_lar.enddir.y; //dir cosines
-        double zdir2_lar= track_lar.enddir.z; //dir cosines
+      double delta_theta = acos(dir_dot / (abs_tms * abs_lar)) * (180/3.14); 
 
-        double z1_tms = track_tms.start.z;   //cm
+      return delta_theta;
+  }
 
-        // Check that this TMS track starts in first few planes; if not move to next track
-        if (z1_tms > 1200){
-          return false;
-        }
+  bool NDLArTMSMatchRecoFiller::Spatial_Match(caf::SRTrack track_lar, caf::SRTrack track_tms, double &xthreshold, double &ythreshold, double &theta_xthreshold, double &theta_ythreshold, double &residual, double &ang_residual) const
+  {
+    double z2_lar = track_lar.end.z;     //cm
 
-        double x1_tms = track_tms.start.x; //cm
-        double y1_tms = track_tms.start.y; //cm
+    // Check LAr track exits through the end; if not move to next track
+    if (z2_lar < 800){
+      return false;
+    }
 
+    // Now get the other variables
+    double x2_lar = track_lar.end.x;     //cm
+    double y2_lar = track_lar.end.y;     //cm
+    double xdir2_lar = track_lar.enddir.x; //dir cosines
+    double ydir2_lar = track_lar.enddir.y; //dir cosines
+    double zdir2_lar= track_lar.enddir.z; //dir cosines
 
-        // Propagate the LAr track into the TMS, by drawing a straight line into the TMS using the 
-        // end direction in the LAr. Propagate the track to the same z as the TMS is, and 
-        // compare the predicted vs actual position in x and y
-        double x_residual = x2_lar + (xdir2_lar / zdir2_lar) * (z1_tms - z2_lar) - x1_tms;
-        double y_residual = y2_lar + (ydir2_lar / zdir2_lar) * (z1_tms - z2_lar) - y1_tms;
+    double z1_tms = track_tms.start.z;   //cm
 
-        return(abs(x_residual) < xthreshold && abs(y_residual) > ythreshold);
+    // Check that this TMS track starts in first few planes; if not move to next track
+    if (z1_tms > 1200){
+      return false;
+    }
+
+    double x1_tms = track_tms.start.x; //cm
+    double y1_tms = track_tms.start.y; //cm
+    double xdir1_tms = track_tms.startdir.x;
+    double ydir1_tms = track_tms.startdir.y;
+    double zdir1_tms = track_tms.startdir.z;
+
+    //calculate the angle between track start and end directions in both the xz- and yz-planes
+    double theta_x = Angular_Match(track_tms.startdir.x, 0, track_tms.startdir.z, track_lar.enddir.x, 0, track_lar.enddir.z);
+    double theta_y = Angular_Match(0, track_tms.startdir.y, track_tms.startdir.z, 0, track_lar.enddir.y, track_lar.enddir.z);
+
+    ang_residual = Angular_Match(track_tms.startdir.x, track_tms.startdir.y, track_tms.startdir.z, track_lar.enddir.x, track_lar.enddir.y, track_lar.enddir.z);
+
+    // Propagate the LAr track into the TMS, by drawing a straight line into the TMS using the 
+    // end direction in the LAr. Propagate the track to the same z as the TMS is, and 
+    // compare the predicted vs actual position in x and y
+    double x_residual = x2_lar + (xdir2_lar / zdir2_lar) * (z1_tms - z2_lar) - x1_tms;
+    double y_residual = y2_lar + (ydir2_lar / zdir2_lar) * (z1_tms - z2_lar) - y1_tms;
+
+    residual = sqrt(pow(x_residual, 2) + pow(y_residual, 2));
+
+    if(abs(x_residual) < xthreshold && abs(y_residual) > ythreshold && abs(theta_x) < theta_xthreshold && abs(theta_y) < theta_ythreshold){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
   void NDLArTMSMatchRecoFiller::_FillRecoBranches(const Trigger &trigger,
@@ -57,27 +101,31 @@ namespace cafmaker
     //thresholds in x and y that define a match
     double xthreshold = 7; //cm
     double ythreshold = 20; //cm
+    double theta_xthreshold = 5; //degrees
+    double theta_ythreshold = 10; //degrees
 
-    for(unsigned int idlp = 0; idlp < sr.nd.lar.ndlp; ++idlp ) {
-      caf::SRNDLArInt dlp = sr.nd.lar.dlp[idlp];
+    std::map<caf::SRNDLArID, caf::SRNDTrackAssn, LarIdComparator> mult_map_spine, mult_map_pandora; // Lar to handle track matching multiplicity
 
-      for(unsigned int ilar = 0; ilar < dlp.ntracks; ++ilar){
-        dlp.tracks[ilar];
-        
-        // Loop over the TMS tracks
-        for(unsigned int ixn_tms = 0; ixn_tms < sr.nd.tms.nixn; ++ixn_tms ) {
-          
-          caf::SRTMSInt tms_int = sr.nd.tms.ixn[ixn_tms];
-          unsigned int n_tms_tracks = tms_int.ntracks;
+    for(unsigned int ixn_tms = 0; ixn_tms < sr.nd.tms.nixn; ixn_tms++){
+      caf::SRTMSInt tms_int = sr.nd.tms.ixn[ixn_tms];
+      unsigned int n_tms_tracks = tms_int.ntracks;
 
-          for(unsigned int itms = 0; itms < n_tms_tracks; itms++){
+      for(unsigned int itms = 0; itms < n_tms_tracks; ++itms){
 
-            // Check matching
-            if (Spatial_Match(dlp.tracks[ilar], tms_int.tracks[itms], xthreshold, ythreshold)) {
-              // Make the match
+        for(unsigned int ixn_dlp = 0; ixn_dlp < sr.nd.lar.ndlp; ixn_dlp++){
+          caf::SRNDLArInt dlp = sr.nd.lar.dlp[ixn_dlp];
+          for (unsigned int ilar = 0; ilar < dlp.ntracks; ++ilar){
+            dlp.tracks[ilar];
+            tms_int.tracks[itms];
+            double residual = 0;
+            double ang_residual = 0;
+
+            if(Spatial_Match(dlp.tracks[ilar], tms_int.tracks[itms], xthreshold, ythreshold, theta_xthreshold, theta_ythreshold, residual, ang_residual))
+            {
               caf::SRNDLArID larid;
-              larid.ixn = idlp;
+              larid.ixn = ixn_dlp;
               larid.idx = ilar;
+              larid.reco = caf::kDeepLearnPhys;
               caf::SRTMSID tmsid;
               tmsid.ixn = ixn_tms;
               tmsid.idx = itms;
@@ -85,20 +133,70 @@ namespace cafmaker
               caf::SRNDTrackAssn match;
               match.larid = larid;
               match.tmsid = tmsid;
-    //          match.transdispl = residual;
-    //          match.angdispl = costheta;
-              sr.nd.trkmatch.extrap.emplace_back(match);
-              //sr.nd.ntrkmatch += 1; //outdated?
-    //          // Can also have multiple track matches, so don't break at the end of the match
-    //          // Should pick the best track match in selection code
+              match.transdispl = residual;
+              match.angdispl = ang_residual;
+
+              if (isnan(mult_map_spine[larid].transdispl))
+                mult_map_spine[larid] = match;
+              else
+              {
+                if (mult_map_spine[larid].transdispl > match.transdispl)
+                  mult_map_spine[larid] = match;
+              }
+            }
+          }
+        }
+
+        for(unsigned int ixn_pandora = 0; ixn_pandora < sr.nd.lar.npandora; ixn_pandora++){
+          caf::SRNDLArInt pandora = sr.nd.lar.pandora[ixn_pandora];
+          for (unsigned int ilar = 0; ilar < pandora.ntracks; ++ilar){
+            pandora.tracks[ilar];
+            tms_int.tracks[itms];
+            double residual = 0;
+            double ang_residual = 0;
+
+            if(Spatial_Match(pandora.tracks[ilar], tms_int.tracks[itms], xthreshold, ythreshold, theta_xthreshold, theta_ythreshold, residual, ang_residual))
+            {
+              caf::SRNDLArID larid;
+              larid.ixn = ixn_pandora;
+              larid.idx = ilar;
+              larid.reco = caf::kPandoraNDLAr;
+              caf::SRTMSID tmsid;
+              tmsid.ixn = ixn_tms;
+              tmsid.idx = itms;
+
+              caf::SRNDTrackAssn match;
+              match.larid = larid;
+              match.tmsid = tmsid;
+              match.transdispl = residual;
+              match.angdispl = ang_residual;
+
+              if (isnan(mult_map_pandora[larid].transdispl))
+                mult_map_pandora[larid] = match;
+              else
+              {
+                if (mult_map_pandora[larid].transdispl > match.transdispl)
+                  mult_map_pandora[larid] = match;
+              }
             }
           }
         }
       }
     }
+
+    for (auto m : mult_map_spine)
+    {
+      sr.nd.trkmatch.extrap.emplace_back(m.second);
+      sr.nd.trkmatch.nextrap += 1;
+    }
+
+    for (auto m : mult_map_pandora)
+    {
+      sr.nd.trkmatch.extrap.emplace_back(m.second);
+      sr.nd.trkmatch.nextrap += 1;
+    }
   }
 
-  // todo: this is a placeholder
   std::deque<Trigger> NDLArTMSMatchRecoFiller::GetTriggers(int triggerType, bool beamOnly) const
   {
     return std::deque<Trigger>();
--- src/reco/NDLArTMSMatchRecoFiller.h
+++ src/reco/NDLArTMSMatchRecoFiller.h
@@ -27,12 +27,18 @@ namespace cafmaker
     private:
       void MatchTracks(caf::StandardRecord &sr) const;
 
-      bool Spatial_Match(caf::SRTrack track_lar, caf::SRTrack track_tms, double &xthreshold, double &ythreshold) const;
+      bool Spatial_Match(caf::SRTrack track_lar, caf::SRTrack track_tms, 
+                          double &xthreshold, double &ythreshold, double &theta_xthreshold, 
+                          double &theta_ythreshold, double &residual, double &ang_residual) const;
 
       void _FillRecoBranches(const Trigger &trigger,
                              caf::StandardRecord &sr,
                              const cafmaker::Params &par,
                              const TruthMatcher *truthMatcher) const override;
+
+      double Angular_Match(double xdir1_tms, double ydir1_tms, 
+                            double zdir1_tms, double xdir2_lar, 
+                            double ydir2_lar, double zdir2_lar) const;
   };
 }
 
