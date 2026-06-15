fce2878457ea168043ab03e7a95221f720c37e23
slanzi00 <samuele.lanzi@cnaf.infn.it>
2026-06-15 17:48:59 +0200

    Try to solve compilation error

--- src/reco/NDLArMINERvAMatchRecoFiller.cxx
+++ src/reco/NDLArMINERvAMatchRecoFiller.cxx
@@ -134,7 +134,7 @@ namespace cafmaker
               match.larid = larid;
               match.minervaid = mnvid;
               match.transdispl = residual;
-              match.cosangdispl = costheta;
+              match.angdispl = costheta;
 
               if (isnan(mult_map_spine[larid].transdispl))
                 mult_map_spine[larid] = match;
@@ -172,7 +172,7 @@ namespace cafmaker
               match.larid = larid;
               match.minervaid = mnvid;
               match.transdispl = residual;
-              match.cosangdispl = costheta;
+              match.angdispl = costheta;
 
               if (isnan(mult_map_pandora[larid].transdispl))
                 mult_map_pandora[larid] = match;
